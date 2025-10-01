7443eafdad698c6f88d56745ac1ec1df37c4b45f
Prikshitsharmacodes <prikshitsharma.codes@gmail.com>
2025-10-01 12:03:43 +0530

    learn about basic data types

---
+++ 02_DataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all js codes as newer version
+
+// alert("hello") wont work in node only in browser
+
+console.log(3
+    +3); // Code Readability is too bad
+
+console.log("Hitesh");
+
+
+
+let name ="Prikshit"
+let age =18
+let isLoggedIn = false
+
+
+// Types of DataTypes
+
+// Primitve-DataTypes
+
+// Number 
+// BigInt
+// String => '' ""
+// Booleans => true/false
+// Null => standalone value
+// Undefined => value hi define ni
+// Symbol => Uniqueness find krne ke liye
+
+// Non-primitve
+//Array
+//Object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
