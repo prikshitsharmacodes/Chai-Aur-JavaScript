f9e6e1646c8e2c0edd6fc33f8ded434ee0f011bf
Prikshitsharmacodes <prikshitsharma.codes@gmail.com>
2025-10-01 16:20:25 +0530

    coversion of data types

---
+++ 03_DataType_Conversion.js
@@ -0,0 +1,31 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "3abc" => NaN  => Type => Number
+// null => 0
+// undefined => NaN
+// True => 1
+// False => 0
+// "Prikshit" => NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLogeedIn = Boolean(isLoggedIn)
+console.log(booleanIsLogeedIn);
+
+
+// 1 => true
+// 0 => false
+
+// "Hitesh" => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+
