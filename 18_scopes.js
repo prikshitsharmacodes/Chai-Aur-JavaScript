b4502190ffa92f146e0eba7ca7d0c92ce854f7af
Prikshitsharmacodes <prikshitsharma.codes@gmail.com>
2025-10-03 16:34:17 +0530

    learnt about global and block scope

---
+++ 18_scopes.js
@@ -0,0 +1,20 @@
+// global scope
+
+var c = 300
+
+if(true){
+    let a =10;
+    const b = 20
+    var c =30
+
+    // block scope
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+// in window global scope different in node
+
+
