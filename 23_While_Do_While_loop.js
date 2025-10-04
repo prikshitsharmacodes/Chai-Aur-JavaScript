2551d5f68aa8e7b1c6e79bf3e1283b8188357465
Prikshitsharmacodes <prikshitsharma.codes@gmail.com>
2025-10-04 17:57:06 +0530

    while loop and do while loop

---
+++ 23_While_Do_While_loop.js
@@ -0,0 +1,25 @@
+let index=0;
+
+while(index<=10){
+    console.log(`Value of index is ${index}`);
+    index+=2;
+}
+
+
+let myArray =["flash","batman","superman"]
+let arr =0;
+while(arr< myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+
+let score = 1;
+
+do{
+    console.log(`Score value ${score}`);
+    score++;
+}
+while(score<=10)
+
+    // Do-while minimum ek bar chalta hi hai
+    
